49a08464c8283dd86c29e3c3f5244b95cabd0cc9
Jared Groves <jgroves27@gmail.com>
2026-06-01 15:12:12 -0400

    refactor(bootstrap): remove thumbprint from oidc-setup.tf
    - Removes the 'thumbprint_list' from the 'aws_iam_openid_connect_provider' resource.
    - Simplifies the OIDC provider configuration, as AWS now automatically
      manages the root CA thumbprint for GitHub's OIDC provider.

--- bootstrap/oidc-setup.tf
+++ bootstrap/oidc-setup.tf
@@ -13,7 +13,6 @@ variable "role_name" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 # The Parameterized IAM Role
